7d1da7d7cc7abff6dc62c89a6ea6132dbd277559
chanwookK <kan0202@naver.com>
2025-12-08 19:23:59 +0900

    [Init] initial commit

---
+++ src/main/java/sa_team8/streaming/FluxStreamingSaApplication.java
@@ -0,0 +1,13 @@
+package sa_team8.streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxStreamingSaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FluxStreamingSaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/sa_team8/streaming/FluxStreamingSaApplicationTests.java
@@ -0,0 +1,13 @@
+package sa_team8.streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxStreamingSaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
